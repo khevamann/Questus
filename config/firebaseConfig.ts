1e0ef05e4e2dbf06712c3267af3ff4962e05a789
Kheva Mann <khevamann@gmail.com>
2020-04-24 14:56:34 -0700

    Create firebaseConfig.ts

---
+++ config/firebaseConfig.ts
@@ -0,0 +1,21 @@
+const firebaseConfig: FireConfig = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+};
+
+type FireConfig = {
+  apiKey: string;
+  authDomain: string;
+  databaseURL: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
+};
+
+export default firebaseConfig;

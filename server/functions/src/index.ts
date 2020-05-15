00e4cdfa75714b08c6cba0ff290448ec4697561c
kheva <khevamann@gmail.com>
2020-05-14 20:09:31 -0700

    Added firebase functions for server side processing

---
+++ server/functions/src/index.ts
@@ -0,0 +1,40 @@
+import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
+import { DocumentSnapshot } from 'firebase-functions/lib/providers/firestore';
+
+admin.initializeApp(functions.config().firebase);
+
+export const createGame = functions.firestore
+  .document('activeGames/{gameId}')
+  .onCreate(async (event: DocumentSnapshot) => {
+    if (event.data() === undefined)
+      throw new Error('Game was not created successfully...');
+
+    /* Generate a game code and increment the game numbers */
+    const gameNum: number = await getGameNumber();
+    return event.ref.update({
+      gameCode: generateGameCode(gameNum),
+    });
+  });
+
+const getGameNumber = async (): Promise<number> => {
+  const configRef = admin.firestore().collection('config').doc('game-config');
+
+  /* Get the current game ID and update it */
+  const docSnap = await configRef.get();
+  configRef.update({ numberGames: docSnap.data()?.numberGames + 1 }).then();
+
+  if (!docSnap.data()) throw new Error('Not sure what happened here...');
+  return docSnap.data()?.numberGames;
+};
+
+export const generateGameCode = (gameNum: number) => {
+  const letters = 'UGTVDWMSYLRKBZIQHPAXNJFEC';
+  const numbers = '271645938';
+  let code = '';
+  code += letters[Math.floor(gameNum) % 25];
+  code += numbers[Math.floor(gameNum / 25) % 9];
+  code += letters[24 - (Math.floor(gameNum / 225) % 25)];
+  code += numbers[Math.floor(gameNum / 5625) % 9];
+  return code;
+};
